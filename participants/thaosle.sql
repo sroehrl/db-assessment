7ef333e8735d1f08bad6a6ff42a179006fb0f357
slethao <126906089+slethao@users.noreply.github.com>
2024-10-13 17:40:00 +0000

    Question one and Question two are anwsered.

---
+++ participants/thaosle.sql
@@ -0,0 +1,19 @@
+# Question 1
+-- The youngest pet is Anna. (birthday is 2024-10)
+    -- Commands:
+        -- SELECT *
+        -- FROM pet
+        -- ORDER BY ASC
+        -- LIMIT 1
+# Question 2
+-- Yes, you would just have to query the records that contains a conditional
+-- that would have filter out any birthdays that happened 3-4 years ago
+--  (there should be 2 records)
+    -- Commands:
+        -- SELECT uuid
+        -- FROM pet
+        -- WHERE dateOfBirth > '2019-08-20' AND dateOfBirth < '2024-10-12'
+# Question 3
+-- 
+# Question 4
+--
